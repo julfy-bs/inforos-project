316bab4b56b159ef457197ad0a64f9feceec3f91
Bogdan Sutuzhko <bogdan.sutuzhko.19@gmail.com>
2020-11-05 11:45:37 +0300

    move commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,67 @@
-const fs = require("fs");
+const PostcssEasyImport = require('postcss-easy-import');
+const Autoprefixer = require('autoprefixer');
+const PostcssAdvancedVariables = require('postcss-advanced-variables');
+const PostcssNested = require('postcss-nested');
+const PostcssRgb = require('postcss-rgb');
+const PostcssInlineSvg = require('postcss-inline-svg');
+const CssNano = require('cssnano');
+const PostcssPxToRem = require('postcss-pxtorem');
+const fs = require('fs');
 
 module.exports = {
-  syntax: "postcss-scss",
+  syntax: 'postcss-scss',
   parser: 'postcss-scss',
   plugins: [
-    require("postcss-easy-import")({
-      extensions: ".pcss"
+    PostcssEasyImport({
+      extensions: '.pcss',
     }),
-    require("autoprefixer")({
-      cascade: false
+    Autoprefixer({
+      cascade: false,
     }),
-    require("postcss-advanced-variables")({
-      variables: JSON.parse(
-        fs.readFileSync("./src/styles/variables.json", "utf-8")
-      )
+    PostcssAdvancedVariables({
+      variables: JSON.parse(fs.readFileSync('./src/styles/variables.json', 'utf-8')),
     }),
-    require("postcss-nested"),
-    require("postcss-rgb"),
-    require("postcss-inline-svg")({
+    PostcssNested,
+    PostcssRgb,
+    PostcssInlineSvg({
       removeFill: false,
-      path: "./src/images/icons"
+      path: './src/images/icons',
     }),
-    require("cssnano"),
-    require("postcss-pxtorem")({
+    CssNano,
+    PostcssPxToRem({
       rootValue: 16,
-      propList: ["*", "!*border*"],
-      selectorBlackList: [/^html$/]
-    })
-  ]
+      propList: ['*', '!*border*'],
+      selectorBlackList: [/^html$/],
+    }),
+  ],
 };
+
+// const fs = require('fs');
+//
+// module.exports = {
+//   syntax: 'postcss-scss',
+//   parser: 'postcss-scss',
+//   plugins: [
+//     require('postcss-easy-import')({
+//       extensions: '.pcss',
+//     }),
+//     require('autoprefixer')({
+//       cascade: false,
+//     }),
+//     require('postcss-advanced-variables')({
+//       variables: JSON.parse(fs.readFileSync('./src/styles/variables.json', 'utf-8')),
+//     }),
+//     require('postcss-nested'),
+//     require('postcss-rgb'),
+//     require('postcss-inline-svg')({
+//       removeFill: false,
+//       path: './src/images/icons',
+//     }),
+//     require('cssnano'),
+//     require('postcss-pxtorem')({
+//       rootValue: 16,
+//       propList: ['*', '!*border*'],
+//       selectorBlackList: [/^html$/],
+//     }),
+//   ],
+// };
